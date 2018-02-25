3fea488d61d660532663e3d5866ee1ed95c31fb5
Adam Zamojski <zamojski@users.noreply.github.com>
2018-02-25 19:24:15 +0100

    Improvements after code review.

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -48,6 +48,8 @@ public class DetailActivity extends AppCompatActivity {
         binding.setSandwich(sandwich);
         Picasso.with(this)
                 .load(sandwich.getImage())
+                .placeholder(R.drawable.ic_image_black_24dp)
+                .error(R.drawable.ic_broken_image_black_24dp)
                 .into(ingredientsIv);
 
         setTitle(sandwich.getMainName());
--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -11,16 +11,24 @@ import java.util.List;
 
 public class JsonUtils {
 
+    private static final String JSON_NAME = "name";
+    private static final String JSON_MAIN_NAME = "mainName";
+    private static final String JSON_ALSO_KNOWN_AS = "alsoKnownAs";
+    private static final String JSON_PLACE_OF_ORIGIN = "placeOfOrigin";
+    private static final String JSON_DESCRIPTION = "description";
+    private static final String JSON_IMAGE = "image";
+    private static final String JSON_INGREDIENTS = "ingredients";
+
     public static Sandwich parseSandwichJson(String json) {
         try {
             JSONObject rootObject = new JSONObject(json);
-            JSONObject nameObject = rootObject.getJSONObject("name");
-            String mainName = nameObject.getString("mainName");
-            List<String> alsoKnownAs = toStringList(nameObject.getJSONArray("alsoKnownAs"));
-            String placeOfOrigin = rootObject.getString("placeOfOrigin");
-            String description = rootObject.getString("description");
-            String image = rootObject.getString("image");
-            List<String> ingredients = toStringList(rootObject.getJSONArray("ingredients"));
+            JSONObject nameObject = rootObject.getJSONObject(JSON_NAME);
+            String mainName = nameObject.optString(JSON_MAIN_NAME);
+            List<String> alsoKnownAs = toStringList(nameObject.getJSONArray(JSON_ALSO_KNOWN_AS));
+            String placeOfOrigin = rootObject.optString(JSON_PLACE_OF_ORIGIN);
+            String description = rootObject.optString(JSON_DESCRIPTION);
+            String image = rootObject.optString(JSON_IMAGE);
+            List<String> ingredients = toStringList(rootObject.getJSONArray(JSON_INGREDIENTS));
             return new Sandwich(mainName, alsoKnownAs, placeOfOrigin, description, image, ingredients);
         } catch (JSONException e) {
             e.printStackTrace();
@@ -34,7 +42,7 @@ public class JsonUtils {
 
         List<String> list = new ArrayList<>();
         for (int i = 0; i < array.length(); i++) {
-            list.add(array.getString(i));
+            list.add(array.optString(i));
         }
         return list;
     }
